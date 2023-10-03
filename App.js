5ad133ee2eb5b7bee9ebb931a54737a6254fac6a
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-03 08:47:25 -0300

    a

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {PermissionsAndroid} from 'react-native';
+  
+
 
 export default function App() {
+
+  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
