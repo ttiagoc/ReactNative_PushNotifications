ab24b56c00be2404cfa929cef4cc3a1e0de58a89
ttiagoc <46878457@comunidadort.edu.ar>
2023-10-22 16:37:56 -0300

    temrinar en polshu

--- App.js
+++ App.js
@@ -1,19 +1,75 @@
-import { useState, useEffect, useRef } from "react";
-import { Text, View, Button, Platform, Image } from "react-native";
-
-import registerNNPushToken from "native-notify";
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from 'expo-status-bar';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import messaging from "@react-native-firebase/messaging"
+import React, { useEffect } from 'react';
 
 export default function App() {
-  registerNNPushToken(13510, 'BvVVG0NNc5qD8cAdqhSyrs');
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission()
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log("Authorization status:", authStatus)
+    }
+  }
+
+  useEffect(() => {
+    if (requestUserPermission()) {
+      messaging().getToken().then(token => {
+        console.log(token)
+      })
+    } else {
+      console.log('Failed token status', authStatus)
+    }
+
+    messaging()
+      .getInitialNotification()
+      .then(async (remoteMessage) => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          )
+        }
+      })
+
+
+    messaging().onNotificationOpenedApp(async (remoteMessage) => {
+      console.log(
+        'Notification caused app to open from background state:',
+        remoteMessage.notification,
+      )
+    })  
+
+    
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background', remoteMessage)
+    })
+
+    const unsubscribe = messaging().onMessage( async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage))
+    })
+
+    return unsubscribe
+
+  }, [])
 
   return (
-    <>
-      <View>
-        <Text> Shalom </Text>
-        <StatusBar style="auto"/>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
